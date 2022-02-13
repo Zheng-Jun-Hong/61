5bb3a160aa50e68544ae9ba68562ed759f9f3d91
ZhengJunHong <richart60308@gmail.com>
2022-02-13 13:51:29 +0800

    20220213 protectiveRelay

--- src/components/station/tabs/stationTabEquip/protectiveRelayTable.vue
+++ src/components/station/tabs/stationTabEquip/protectiveRelayTable.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="smart_device_data_container">
-        <div class="select" v-show="true">
-            <el-select v-model="value" placeholder="請選擇" style="width:200px" @change="pv_protective_relay_data">
+        <div class="select" v-show="false">
+            <el-select v-model="value" placeholder="請選擇" style="width:200px" @change="selection">
                 <el-option
                     v-for="item in options"
                     :key="item.label"
@@ -14,7 +14,7 @@
         <br/>
         <el-table
             class="header_class"
-            :data="protectiveRelayData"
+            :data="protectiveRelayDataReg"
             empty-text="無資料"
             stripe
             :header-cell-style="styleHeader"
@@ -135,6 +135,7 @@ export default {
                 }
             ],
             protectiveRelayData:[],
+            protectiveRelayDataReg: [],
             data_reg: [],
             ieeeData: [],
             value: "全部",
@@ -142,34 +143,42 @@ export default {
             outputLabel: "全部",
             ieeeCode: "total",
             data_type: "total",
+            dataType: "total",
             title: "保護電驛點位資料",
             total: "total"
         }
     },
     methods:{
-        pv_protective_relay_data(val){
+        selection(val){
+            this.dataType = "total";
+            this.data_reg = [];
+            this.protectiveRelayDataReg = [];
+            this.pv_protective_relay_data(val);
+        },
+        pv_protective_relay_data(val, dataType=this.dataType){
             // console.log(this.parentBtnDeviceName)   //設備名稱
             // console.log(this.parentBtnDeviceId)    //設備ID
             // console.log(this.parentBtnDeviceCollection)    //設備collection
-            this.data_type = this.total;
+
+            this.data_type = dataType;
             this.ieeeCode = val[0];
 
-            let obj = this.options.find((item)=>{
-                return item.value === val;
-            });
-            this.label = obj.label;
+            // let obj = this.options.find((item)=>{
+            //     return item.value === val;
+            // });
+            // this.label = null;
             this.outputLabel = "全部"
 
             let that = this;
             that.protectiveRelayData = [];
-            that.data_reg = [];
             let time = 0;
 
             const promise = new Promise(function(resolve, reject){
+                that.data_reg = [];
                 that.ieeeData  = [];
                 resolve('success');
             })
-            promise.then(function(){
+            promise.then(() =>{
                 function getData(time){
                     if(time < val.length){
                         that.axios.post("/pv_protective_relay_data",{
@@ -177,8 +186,10 @@ export default {
                             "IEEE_code": val[time]
                         })
                         .then(function(data){
+                            if (time === 0 && that.data_reg.length > 0){
+                                that.data_reg = [];
+                            }
                             that.ieeeData.push(data.data.data.relay_data);
-                            // console.log(that.ieeeData);
                             that.data_reg = that.data_reg.concat(data.data.data.relay_data);
                             time += 1;
                             if (time+1 > val.length){
@@ -225,13 +236,14 @@ export default {
         tableData(){
             let relay_data = this.data_reg;
             let data_length = relay_data.length;
-            for (var i = 0; i <= data_length; i+=2){
+            this.protectiveRelayData = [];
+            for (var i = 0; i < data_length; i+=2){
                 if (i + 1 >= data_length){
                     this.protectiveRelayData.push({
                         label: relay_data[i]["label"],
+                        label1: null,
                         LN: relay_data[i]["LN"],
                         Op: relay_data[i]["Attribute"]["Op"],
-                        label1: null,
                         LN1: null,
                         Op1: null
                     })
@@ -247,7 +259,10 @@ export default {
                     })
                 }
             }
-            console.log(this.protectiveRelayData);
+            this.protectiveRelayDataReg = this.protectiveRelayData;
+            console.log("update");
+            //console.log(this.protectiveRelayDataReg);
+            //console.log(this.protectiveRelayData);
         },
         dblclickHandler(row, column){
             var comfirm = false;
@@ -257,6 +272,7 @@ export default {
                     for (var j=0; j<this.ieeeData[i].length; j++){
                         if (this.ieeeData[i][j]["label"] == row.label){
                             this.data_type = row.LN;
+                            this.dataType = row.LN;
                             this.ieeeCode = this.value[i];
                             comfirm = true;
                             break
@@ -273,6 +289,7 @@ export default {
                     for (var j=0; j<this.ieeeData[i].length; j++){
                         if (this.ieeeData[i][j]["label"] == row.label1){
                             this.data_type = row.LN1;
+                            this.dataType = row.LN1;
                             this.ieeeCode = this.value[i];
                             comfirm = true;
                             break
@@ -283,29 +300,44 @@ export default {
                     }
                 }
             }
-            
+            if (this.ieeeCode == "全" || this.ieeeCode == "全部" || this.ieeeCode == "部"){
+                this.ieeeCode = "total";
+            }
         },
-
         dblclickHeader(){
-            // if (column.label == "保護電驛點位資料"){
-            //     this.pv_protective_relay_data(["total"]);
-            // }
-            alert("abc");
-            // alert(column);
+            this.data_type = "total";
+            this.outputLabel = "全部";
+            this.dataType  = "total";
         }
     },
+    // beforeUpdate(){
+    //     if (typeof(this.value) === "string"){
+    //         this.value = ["total"];
+    //     }
+    //     // this.protectiveRelayDataReg = this.protectiveRelayData;
+    // },
+    // updated(){
+    //     this.protectiveRelayDataReg = this.protectiveRelayData;
+    // },
     mounted(){
         // alert('頁面維護中')   //完成時拿掉
-        let that = this
-        this.pv_protective_relay_data(["total"])
-        let headerEvent = document.querySelector('.header_class')
-        headerEvent.addEventListener("click", this.dblclickHeader)
+        let that = this;
+        this.pv_protective_relay_data(["total"]);
+        let headerEvent = document.querySelector(".header_class .el-table__header tr");
+        headerEvent.addEventListener("dblclick", this.dblclickHeader);
         this.syncData = setInterval(function(){
-            that.pv_protective_relay_data(that.value)
-        }, 1000)
+            if (that.value === "全部"){
+                that.pv_protective_relay_data(["total"]);
+            }
+            else{
+                that.pv_protective_relay_data(that.value);
+            }
+        }, 5000);
     },
     destroyed(){
-        clearInterval(this.syncData)
+        clearInterval(this.syncData);
+        let headerEvent = document.querySelector(".header_class .el-table__header tr");
+        headerEvent.removeEventListener("dblclick", this.dblclickHeader);
     }
 }
 </script>
